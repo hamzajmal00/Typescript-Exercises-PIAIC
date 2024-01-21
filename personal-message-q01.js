27c6179c8fb730f3ec862ddbfcdff03717668739
unknown <tempmanagebyhamza@gmail.com>
2024-01-21 11:15:30 +0500

    Q1: Resolved Q1

---
+++ personal-message-q01.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = 'hamza ajmal';
+console.log("Hello ".concat(name, ", would you like to learn some Python today?"));
---
+++ personal-message-q01.ts
@@ -0,0 +1,4 @@
+const name: string = 'hamza ajmal';
+console.log(`Hello ${name}, would you like to learn some Python today?`);
+
+export {};
